fa42dfee174cfe05fc703f2d6d770d542fb17066
Connor Thurston <connor.thurston1@gmail.com>
2023-03-01 00:43:09 -0700

    counter deploy

--- src/index.ts
+++ src/index.ts
@@ -30,14 +30,28 @@ async function run() {
   const contract = new ethers.Contract(
     address,
     [
-      "function hello() public pure returns(string memory)",
+      "function count() public",
+      "function getCounter() public view return (uint32)"
     ],
     // @ts-ignore
     provider
   );
   console.log("Run it!!!");
   // document.body.innerHTML = await contract.hello();
-  console.log(await contract.hello());
+  const el = document.createElement("div");
+  async function setCounter() {
+    el.innerHTML = await contract.getCounter();
+  }
+  setCounter();
+  const button = document.createElement("button");
+  button.innerText = "increment";
+  button.onclick = async function() {
+    await contract.count();
+    setCounter();
+  }
+  document.body.appendChild(el);
+  document.body.appendChild(button);
+  // console.log(await contract.hello());
 }
 
 run();
